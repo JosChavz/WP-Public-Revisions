3cf5480ae9b8e22a3592c9c12b5a3bbbb4266e14
Hozay <josmchavz@gmail.com>
2026-05-29 10:40:26 -0700

    hotfix: only run the JS code if the button exists in the frontend

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,7 +1,9 @@
 const toggleButton = document.getElementsByClassName("wppr-previous-button")[0];
 const historyBox = document.getElementsByClassName("wppr-history-box")[0];
 
-toggleButton.addEventListener("click", () => {
-  const style = getComputedStyle(historyBox);
-  historyBox.style.display = style.display == "none" ? "block" : "none";
-});
+if (!!toggleButton) {
+    toggleButton.addEventListener("click", () => {
+        const style = getComputedStyle(historyBox);
+        historyBox.style.display = style.display === "none" ? "block" : "none";
+    });
+}
--- wp-public-revisions.php
+++ wp-public-revisions.php
@@ -3,7 +3,7 @@
 	 * Plugin Name: WP Public Revisions
 	 * Plugin URI: https://github.com/joschavz/wp-public-revisions
 	 * Description: Display a public revision history on any post or page via the [revision_history] shortcode. Visitors can click through to read any past version of the content.
-	 * Version: 1.2.1
+	 * Version: 1.2.2
 	 * Requires PHP: 8.3
 	 * Author: Jose Manuel Chavez
 	 * Author URI: https://hozay.tech/
